62753da4b29413d15700e8f562f9553aeef75e36
rongxanh88 <rongxanh88@gmail.com>
2017-04-18 09:41:50 -0600

    passed all iteration 1 tests for pantry class

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,24 @@
 class Pantry
+  attr_accessor :stock
 
+  def initialize
+    @stock = {}
+  end
+  
+  def stock_check(food)
+    if stock[food] != nil
+      return stock[food]
+    else
+      return 0
+    end
+  end
+
+  def restock(food, amount)
+    if stock.has_key?(food)
+      previous = stock[food]
+      stock[food] = previous + amount
+    else
+      stock[food] = amount
+    end
+  end
 end
